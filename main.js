cef59b238bcd3c9a5583b309ff3e61be2ddae76a
DESKTOP-GVB5T3L\Twinny43_ARO <aro1004@twinny.co.kr>
2018-09-10 17:45:42 +0900

    좌표가 테두리에 있으면 false를 리턴하는 함수

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ $(function () {
     function setMap() {
         var mapSize = parseInt($("#mapSize option:selected").html());
         var shipAmt = parseInt($("#shipAmt option:selected").html());
-        console.log(mapSize, shipAmt, typeof (mapSize))
 
         var contents;
         for (var i = 0; i < mapSize; i++) {
@@ -23,16 +22,30 @@ $(function () {
     function getMapArr() {
         var mapArr = [];
         var mapSize = setMap()[0];
-        console.log("map", mapSize)
         for (var i = 0; i < mapSize; i++) {
             for (var j = 0; j < mapSize; j++) {
                 mapArr.push("" + i + "" + j)
             }
         }
-        console.log(mapArr);
         return mapArr;
     }
 
+    function definePos() {
+        var mapArr = getMapArr();
+        var mapSize = setMap()[0];
+        var mapSizePow = Math.pow(mapSize, 2.0);
+        //테두리에 있으면 false, 중앙에 있으면 true를 리턴
+        var isCenter = [];
+        for (var i = 0; i < mapSizePow; i++) {
+            if (mapArr[i].indexOf(0) >= 0 || mapArr[i].indexOf(mapSize - 1) >= 0) {
+                isCenter[i] = false;
+            } else {
+                isCenter[i] = true;
+            }
+        }
+        return isCenter;
+    }
+
     $("#startBtn").click(setMap)
-    $("#alignBtn").click(getMapArr);
+    $("#alignBtn").click(definePos);
 })
