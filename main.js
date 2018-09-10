f278cbd3970f087b1ba092a43c5af31976a2f66f
DESKTOP-GVB5T3L\Twinny43_ARO <aro1004@twinny.co.kr>
2018-09-10 18:11:41 +0900

    테두리 판단을 위해 맵의 크기를 키움 (맵 크기가 7일 때 9x9로 만듦)

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ $(function () {
         var shipAmt = parseInt($("#shipAmt option:selected").html());
 
         var contents;
-        for (var i = 0; i < mapSize; i++) {
+        for (var i = 0; i < mapSize + 2; i++) {
             contents += "<tr>"
-            for (var j = 0; j < mapSize; j++) {
+            for (var j = 0; j < mapSize + 2; j++) {
                 contents += "<td>"
                 contents += "" + i + "" + j;
                 contents += "</td>"
@@ -22,30 +22,15 @@ $(function () {
     function getMapArr() {
         var mapArr = [];
         var mapSize = setMap()[0];
-        for (var i = 0; i < mapSize; i++) {
-            for (var j = 0; j < mapSize; j++) {
+        for (var i = 0; i < mapSize + 2; i++) {
+            for (var j = 0; j < mapSize + 2; j++) {
                 mapArr.push("" + i + "" + j)
             }
         }
+        console.log(mapArr)
         return mapArr;
     }
 
-    function definePos() {
-        var mapArr = getMapArr();
-        var mapSize = setMap()[0];
-        var mapSizePow = Math.pow(mapSize, 2.0);
-        //테두리에 있으면 false, 중앙에 있으면 true를 리턴
-        var isCenter = [];
-        for (var i = 0; i < mapSizePow; i++) {
-            if (mapArr[i].indexOf(0) >= 0 || mapArr[i].indexOf(mapSize - 1) >= 0) {
-                isCenter[i] = false;
-            } else {
-                isCenter[i] = true;
-            }
-        }
-        return isCenter;
-    }
-
     $("#startBtn").click(setMap)
-    $("#alignBtn").click(definePos);
+    $("#alignBtn").click(getMapArr);
 })
